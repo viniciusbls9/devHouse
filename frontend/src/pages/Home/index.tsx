3df9cbd952697cb57a02a915b4b528a84e21f55c
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-09 08:42:39 -0300

    Titulo da home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,11 +4,15 @@ import Header from '../../components/Header';
 import './styles.css';
 
 const Home: React.FC = () => {
-  return (
-    <div className="page-home">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="page-home">
+            <Header />
+
+            <div className="content-home">
+                <h1>Reserve casas, hotéis e muito mais...</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
