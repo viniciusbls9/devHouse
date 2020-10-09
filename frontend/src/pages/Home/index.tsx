d517eb5e4389882408f89a51445f449a5d6a019c
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-09 09:09:40 -0300

    Home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '../../components/Header';
 
+import foto from '../../assets/foto.jpg';
+
 import './styles.css';
 
 const Home: React.FC = () => {
@@ -10,6 +12,13 @@ const Home: React.FC = () => {
 
             <div className="content-home">
                 <h1>Reserve casas, hotéis e muito mais...</h1>
+
+                <div className="houses-content">
+                    <div className="house-infos">
+                        <img src={foto} alt=""/>
+                        <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
