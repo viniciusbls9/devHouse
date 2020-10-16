beb73ee6e5a1208b0dcaf25582cbcb475ab6c4b5
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-16 11:49:11 -0300

    Frontend página Home finalizada

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -31,33 +31,51 @@ const Home: React.FC = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className="single-house">
+                        <div className="house-infos">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Marker" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>Reservar</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
 
                 <h2>Minhas Reservas</h2>
                 <div className="reserves">
-                    {/* <div className="single-reserve"> */}
-                        <div className="left-side">
-                            <img src={foto} alt="" />
-                        </div>
-
-                        <div className="right-side">
-                            <div className="reserve-infos">
-                                <img src={calendar} alt="Marker" />
-                                <span>20 Fevereiro 2020</span>
+                    <div className="single-reserve">
+                        <div className="reserves-infos">
+                            <div className="left-side">
+                                <img src={foto} alt="" />
                             </div>
 
-                            <div className="reserve-infos">
-                                <img src={marker} alt="Marker" />
-                                <span>20 Fevereiro 2020</span>
+                            <div className="right-side">
+                                <div className="reserve-infos">
+                                    <img src={calendar} alt="Marker" />
+                                    <span>20 Fevereiro 2020</span>
+                                </div>
+
+                                <div className="reserve-infos">
+                                    <img src={marker} alt="Marker" />
+                                    <span>20 Fevereiro 2020</span>
+                                </div>
+
+                                <button>Cancelar</button>
                             </div>
-
-                            <button>Cancelar</button>
                         </div>
-                    {/* </div> */}
+                    </div>
 
-
+                    
                 </div>
+
             </div>
         </div>
     );
