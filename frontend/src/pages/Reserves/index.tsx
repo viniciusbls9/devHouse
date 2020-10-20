7cff0b409e369edd3ad256b45395830975a30f88
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-20 11:01:49 -0300

    Ajustes na página Reserves

--- frontend/src/pages/Reserves/index.tsx
+++ frontend/src/pages/Reserves/index.tsx
@@ -16,37 +16,147 @@ const MyHouses: React.FC = () => {
                 <h1>Reserve casas, hotéis e muito mais...</h1>
 
                 <div className="houses-content">
-                    <div className="house-infos-reserves">
-                        
-                        <img src={foto} alt="" />
-                        <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
-                        <div className="house-address">
-                            <img src={marker} alt="Lixeira" />
-                            <span>Avenida 19, São Paulo</span>
-                        </div>
-                        <div className="house-value-reserves">
-                            <span>R$ 1000,00 / Dia</span>
-                            <button>
-                                <img src={trash} alt=""/>
-                            </button>
-                            <button>Reservar</button>
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="house-infos-reserves">
-                        
-                        <img src={foto} alt="" />
-                        <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
-                        <div className="house-address">
-                            <img src={marker} alt="Lixeira" />
-                            <span>Avenida 19, São Paulo</span>
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
                         </div>
-                        <div className="house-value-reserves">
-                            <span>R$ 1000,00 / Dia</span>
-                            <button>
-                                <img src={trash} alt=""/>
-                            </button>
-                            <button>Reservar</button>
+                    </div>
+
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="single-reserve">
+                        <div className="house-infos-reserves">
+                            <img src={foto} alt="" />
+                            <span>Casa bem grande com piscina aquecida, com 3 quartos e 1 suite</span>
+                            <div className="house-address">
+                                <img src={marker} alt="Lixeira" />
+                                <span>Avenida 19, São Paulo</span>
+                            </div>
+                            <div className="house-value-reserves">
+                                <span>R$ 1000,00 / Dia</span>
+                                <button>
+                                    <img src={trash} alt="" />
+                                </button>
+                                <button>Reservar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
